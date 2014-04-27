3ed601951f6cd9a1ddf8e9f0e8d04571f7460679
Marco Koch <mkoch@marco-koch.net>
2014-04-27 17:01:53 +0200

    Implementation of task 5

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,10 @@
 ################################################################
 ##setwd("/home/marco/DataScience/data/GetAndClean")
 
+library(reshape2)
 ##Reading basic information
 message("Reading basic information")
-activity <- read.table("activity_labels.txt", col.names=c("ID", "Activity"),colClasses = "character");
+activity <- read.table("activity_labels.txt", col.names=c("ColumnID", "Activity"),colClasses = "character");
 features <- read.table("features.txt", col.names=c("ColumnID", "ColumnName"));
 columnHeader <- features[,"ColumnName"];
 mean_std <- c(".*mean.*", ".*std.*");
@@ -12,26 +13,27 @@ columnToUse <- which(is.element(features$ColumnName, mean_std_header))
 
 ##Reading the subject, acitivity and test data
 message("Reading test data")
-subject_test <- read.table(paste("test","/","subject_test.txt", sep=""))
-activity_test <- read.table(paste("test","/","y_test.txt", sep=""), col.names=c("Activity"))
-x_test <- read.table(paste("test","/","X_test.txt", sep=""))
+subject_test <- read.table(file.path("test","/","subject_test.txt"))
+activity_test <- read.table(file.path("test","/","y_test.txt"), col.names=c("Activity"))
+x_test <- read.table(file.path("test","/","X_test.txt"))
 
 ##Reading the subject, acitivity and training data
 message("Reading training data")
-subject_train <- read.table(paste("train","/","subject_train.txt", sep=""))
-activity_train <- read.table(paste("train","/","y_train.txt", sep=""), col.names=c("Activity"))
-x_train <- read.table(paste("train","/","X_train.txt", sep=""))
+subject_train <- read.table(file.path("train","/","subject_train.txt"))
+activity_train <- read.table(file.path("train","/","y_train.txt"), col.names=c("Activity"))
+x_train <- read.table(file.path("train","/","X_train.txt"))
 
 ## Create data sets
-message("Creating data sets")
+message("Processing data")
 activity_test <- factor(x=activity_test$Activity,labels=activity$Activity,ordered=TRUE)
 activity_train <- factor(x=activity_train$Activity,labels=activity$Activity,ordered=TRUE)
-
-message("Creating the mean and standard deviation data set")
 x_test_result <- x_test[,columnToUse]
+x_train_result <- x_train[,columnToUse]
+
+##############################################################################################################
+message("Creating the mean and standard deviation data set")
 x_test_result <- cbind(activity_test, subject_test,x_test_result)
 names(x_test_result) <- c("Activity", "Subject", mean_std_header)
-x_train_result <- x_train[,columnToUse]
 x_train_result <- cbind(activity_train, subject_train,x_train_result)
 names(x_train_result) <- c("Activity", "Subject", mean_std_header)
 UCIHARDataset <- rbind(x_test_result,x_train_result)
@@ -41,11 +43,12 @@ message(" Columns: " , ncol(UCIHARDataset), ")")
 
 ##############################################################################################################
 message("Creating the average data set")
-x_test_result_2 <- cbind(activity_test, subject_test,x_test)
-names(x_test_result_2) <- c("Activity", "Subject", mean_std_header)
-x_train_result_2 <- cbind(activity_train, subject_train,x_train)
-names(x_train_result_2) <- c("Activity", "Subject", mean_std_header)
-AverageDataset <- rbind(x_test_result_2, x_train_result_2)
+AverageDataset <- melt(UCIHARDataset, meassure.vars=mean_std_header, id=c("Activity", "Subject"))
+AverageDataset <- dcast(AverageDataset, Activity + Subject ~ variable, mean)
 message("Name: AverageDataset")
 message("(Rows: " , nrow(AverageDataset), appendLF=FALSE)
 message(" Columns: " , ncol(AverageDataset), ")")
+message("Writing CSV output file to UCIHARDataset.txt")
+write.csv(UCIHARDataset, file="UCIHARDataset.txt")
+message("Writing CSV output file to AverageDataset.txt")
+write.csv(AverageDataset, file="AverageDataset.txt")
